e39c1c0f22dd762430800860aed33925d72b83da
0x41gawor <41gawor@gmail.com>
2022-02-08 23:02:49 +0100

    feat(service): create item service class

---
+++ src/main/kotlin/pl/gawor/vapecatalogue/service/ItemService.kt
@@ -0,0 +1,27 @@
+package pl.gawor.vapecatalogue.service
+
+import pl.gawor.vapecatalogue.model.ItemModel
+import pl.gawor.vapecatalogue.repository.ItemRepository
+
+class ItemService(private val repository: ItemRepository) : CRUDService<ItemModel> {
+
+    override fun list(): List<ItemModel>? {
+        return repository.list()
+    }
+
+    override fun create(model: ItemModel): ItemModel {
+        return repository.create(model)
+    }
+
+    override fun read(id: Int): ItemModel {
+        return repository.read(id)
+    }
+
+    override fun update(id: Int, model: ItemModel): ItemModel {
+        return repository.update(id, model)
+    }
+
+    override fun delete(id: Int): Boolean {
+        return repository.delete(id)
+    }
+}
