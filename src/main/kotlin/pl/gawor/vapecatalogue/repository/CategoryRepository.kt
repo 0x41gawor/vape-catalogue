86ff9b1f90451c1b2dd3c0e9921ecefb0f63bddb
0x41gawor <41gawor@gmail.com>
2022-02-08 22:19:14 +0100

    feat(repository): create category repository class

---
+++ src/main/kotlin/pl/gawor/vapecatalogue/repository/CategoryRepository.kt
@@ -0,0 +1,64 @@
+package pl.gawor.vapecatalogue.repository
+
+import pl.gawor.vapecatalogue.model.CategoryModel
+import pl.gawor.vapecatalogue.repository.dbhelper.DbHelper
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+
+class CategoryRepository : CRUDrepository<CategoryModel> {
+
+    private val dbHelper = DbHelper()
+
+    override fun list(): List<CategoryModel>? {
+        val list = ArrayList<CategoryModel>()
+        val connection = dbHelper.getConnection()
+        val query =  "select * from category"
+        val statement: Statement
+        val resultSet: ResultSet
+
+        try {
+            statement = connection!!.createStatement()
+            resultSet = statement.executeQuery(query)
+            var model: CategoryModel?
+            while (resultSet.next()) {
+                model = CategoryModel(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                )
+                println(model)
+                list.add(model)
+            }
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+        }
+        return list
+    }
+
+    override fun create(entity: CategoryModel): CategoryModel {
+        val query = "insert into category values (${entity.id}, '${entity.name}')"
+        dbHelper.executeQuery(query)
+        //TODO get created model
+        return entity
+    }
+
+    override fun read(id: Int): CategoryModel {
+        //TODO implement
+        return CategoryModel(0,"")
+    }
+
+    override fun update(id: Int, entity: CategoryModel): CategoryModel {
+        val query = "update category set name = '${entity.name}' where id = $id"
+        dbHelper.executeQuery(query)
+        //TODO get updated model
+        return entity
+    }
+
+    override fun delete(id: Int): Boolean {
+        val query = "delete from book where id = $id"
+        dbHelper.executeQuery(query)
+        //TODO return true if deletion is completed
+        //TODO return true if deletion is completed
+        return true
+    }
+}
