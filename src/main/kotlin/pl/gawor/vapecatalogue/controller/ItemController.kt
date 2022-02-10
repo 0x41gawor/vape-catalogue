fef73ca48f6512cf2cd14772c56fb1d50c63153d
0x41gawor <41gawor@gmail.com>
2022-02-10 22:12:16 +0100

    feat(controller): implement item controller

--- src/main/kotlin/pl/gawor/vapecatalogue/controller/ItemController.kt
+++ src/main/kotlin/pl/gawor/vapecatalogue/controller/ItemController.kt
@@ -2,7 +2,10 @@ package pl.gawor.vapecatalogue.controller
 
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+import javafx.scene.image.Image
 import javafx.scene.image.ImageView
+import pl.gawor.vapecatalogue.VapeCatalogueApplication
+import pl.gawor.vapecatalogue.model.ItemModel
 
 class ItemController {
     @FXML
@@ -13,4 +16,14 @@ class ItemController {
 
     @FXML
     private lateinit var label_price: Label
+
+    private lateinit var model: ItemModel
+
+    fun set(model: ItemModel) {
+        this.model = model
+        label_name.text = model.name
+        label_price.text = model.price.toString()
+        val image = Image(VapeCatalogueApplication::class.java.getResourceAsStream(model.imagePath))
+        imageView.image = image
+    }
 }
