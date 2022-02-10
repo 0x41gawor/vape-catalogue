dd1c4b3d3c97f593a28f01ffc1e32139ed60896b
0x41gawor <41gawor@gmail.com>
2022-02-10 22:40:33 +0100

    feat(service): add list by category method

--- src/main/kotlin/pl/gawor/vapecatalogue/service/ItemService.kt
+++ src/main/kotlin/pl/gawor/vapecatalogue/service/ItemService.kt
@@ -1,11 +1,12 @@
 package pl.gawor.vapecatalogue.service
 
+import pl.gawor.vapecatalogue.model.CategoryModel
 import pl.gawor.vapecatalogue.model.ItemModel
 import pl.gawor.vapecatalogue.repository.ItemRepository
 
 class ItemService(private val repository: ItemRepository) : CRUDService<ItemModel> {
 
-    override fun list(): List<ItemModel>? {
+    override fun list(): List<ItemModel> {
         return repository.list()
     }
 
@@ -23,5 +24,9 @@ class ItemService(private val repository: ItemRepository) : CRUDService<ItemMode
 
     override fun delete(id: Int): Boolean {
         return repository.delete(id)
+    }
+
+    fun list(category: CategoryModel): List<ItemModel> {
+        return repository.list(category)
     }
 }
