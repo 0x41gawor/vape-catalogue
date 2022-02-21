da8e32da1d5746ca6b35bb3a8efc83aa7966f530
0x41gawor <41gawor@gmail.com>
2022-02-21 22:30:58 +0100

    feat(model): add default constructor for item model

--- src/main/kotlin/pl/gawor/vapecatalogue/model/ItemModel.kt
+++ src/main/kotlin/pl/gawor/vapecatalogue/model/ItemModel.kt
@@ -1,10 +1,10 @@
 package pl.gawor.vapecatalogue.model
 
 data class ItemModel(
-    val id: Int,
-    val name: String,
-    val price: Double,
-    val imagePath: String,
-    val notes: String,
-    val categoryId: Int
+    val id: Int = 0,
+    val name: String = "",
+    val price: Double = 0.0,
+    val imagePath: String = "",
+    val notes: String = "",
+    val categoryId: Int = 0
 )
