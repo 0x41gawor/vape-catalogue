040abbe97043a2d9402f0b2035485b691175951d
0x41gawor <41gawor@gmail.com>
2022-02-22 20:18:53 +0100

    feat(Application): create Main method, so it can be deployable with shaded plugin #1

---
+++ src/main/kotlin/pl/gawor/vapecatalogue/Main.kt
@@ -0,0 +1,9 @@
+package pl.gawor.vapecatalogue
+
+class Main {
+    companion object {
+        @JvmStatic fun main(args:Array<String>) {
+            VapeCatalogueApplication.main(args)
+        }
+    }
+}
